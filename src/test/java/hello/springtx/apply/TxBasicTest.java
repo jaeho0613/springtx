1f6ea35a0c0975b4b6b2f90221c69368b45ff28a
jaeho0613 <ywm2004@gmail.com>
2024-10-11 22:09:17 +0900

    섹션10 프로젝트 셋팅

---
+++ src/test/java/hello/springtx/apply/TxBasicTest.java
@@ -0,0 +1,11 @@
+package hello.springtx.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+public class TxBasicTest {
+    
+    
+}
