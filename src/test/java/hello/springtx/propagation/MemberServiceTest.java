c32340869fc9d536862ecd4ae6ff5257f07afbe4
jaeho0613 <ywm2004@gmail.com>
2024-10-13 00:40:43 +0900

    섹션12 - 트랜잭션 전파 활용3 - 단일 트랜잭션

--- src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -1,6 +1,7 @@
 package hello.springtx.propagation;
 
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -25,6 +26,42 @@ class MemberServiceTest {
      */
     @Test
     void outerTxOff_success() {
+        // Given
+        String username = "outerTxOff_success";
+
+        // When
+        memberService.joinV1(username);
+
+        // Then : 모든 데이터가 저장 저장된다.
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
+    }
+
+    /**
+     * memberService    @Transactional:OFF
+     * memberRepository @Transactional:ON
+     * logRepository    @Transactional:ON Exception
+     */
+    @Test
+    void outerTxOff_fail() {
+        // Given
+        String username = "로그예외_outerTxOff_fail";
+
+        // When
+        Assertions.assertThatThrownBy(() -> memberService.joinV1(username)).isInstanceOf(RuntimeException.class);
+
+        // Then : 모든 데이터가 저장 저장된다.
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isEmpty());
+    }
+
+    /**
+     * memberService    @Transactional:ON
+     * memberRepository @Transactional:OFF
+     * logRepository    @Transactional:OFF
+     */
+    @Test
+    void singleTx() {
         // Given
         String username = "outerTxOff_success";
 
